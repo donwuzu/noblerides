a018eeade1440655e19c4c79c93fb53a56c4ea60
dave <davmurage@gmail.com>
2025-08-03 10:46:30 +0300

    removed view.name

--- app/Mail/BookingConfirmation.php
+++ app/Mail/BookingConfirmation.php
@@ -31,30 +31,12 @@ class BookingConfirmation extends Mailable
     /**
      * Get the message envelope.
      */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Booking Confirmation',
-        );
-    }
+   
 
     /**
      * Get the message content definition.
      */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
+   
 
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
-    }
+  
 }
